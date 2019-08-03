4ff6bfec5bca54197cd7d44299d138c7023208fb
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-08-03 18:06:34 +0200

    nuevos prefabs de los botones

---
+++ GMTK_JAM_2019_UNITYPROJ/Assets/Prefabs/Button/rotateButtonBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateButtonBase : MonoBehaviour
+{
+    [SerializeField] Vector3 localAxis;
+    [SerializeField] float speed = 10f;
+    Vector3 axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axis = transform.localToWorldMatrix * localAxis;
+
+        transform.RotateAround(transform.position, axis, speed * Time.deltaTime);
+    }
+}
