d1d567f0bbf06bee9e19355b2c6d3b2672c0f6ea
Daoab <38359000+Daoab@users.noreply.github.com>
2019-08-04 16:49:09 +0200

    Cambiado canvas para que escale bien, ahora al final de los creditos vuelves al menu

---
+++ GMTK_JAM_2019_UNITYPROJ/Assets/BackToMenuOnAwake.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMenuOnAwake : MonoBehaviour
+{
+    private void Start()
+    {
+        FindObjectOfType<SceneTransitionManager>().LoadScene("MainMenu");
+    }
+}
