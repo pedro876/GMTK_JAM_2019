08b5cb2ff2c45e20dd54bb554b61d1a76a5e0307
Daoab <38359000+Daoab@users.noreply.github.com>
2019-08-04 17:41:56 +0200

    Añadida musica de creditos y de juego

--- GMTK_JAM_2019_UNITYPROJ/Assets/Scripts/GameMusic.cs
+++ GMTK_JAM_2019_UNITYPROJ/Assets/Scripts/GameMusic.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestoyOnLoad : MonoBehaviour
+public class GameMusic : MonoBehaviour
 {
     void Awake()
     {
---
+++ GMTK_JAM_2019_UNITYPROJ/Assets/Scripts/PlayCreditsMusic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCreditsMusic : MonoBehaviour
+{
+    private void Awake()
+    {
+        FindObjectOfType<GameMusic>().GetComponent<AudioSource>().Stop();
+    }
+}
